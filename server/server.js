4c9a43b1f0deaef5c268426f688f743bf48628cc
chuck-PC\chuck <chuck@webfitsolutions.com>
2018-04-22 19:10:53 -0500

    add new message and create message events, move js to its own file

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New user connected.')
 
+  socket.emit('newMessage', {
+    from: 'bob',
+    text: 'what up?',
+    createdAt: 123
+  })
+
+  socket.on('createMessage', (message) => {
+    console.log('create message', message)
+  })
+
   socket.on('disconnect', () => {
     console.log('Disconnected from client')
   })
